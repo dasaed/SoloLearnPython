703c6d7a2d28a075c5eb153827c79b6608bbc78b
Dasaed <carlos.dasaed@gmail.com>
2019-01-12 14:17:20 -0330

    updating

--- Section2_Functions_and_Modules/handlingExceptions.py
+++ Section2_Functions_and_Modules/handlingExceptions.py
@@ -15,4 +15,21 @@ except ZeroDivisionError:
     print("You are trying a division by 0")
 except (ValueError,TypeError):
     print("an error occured")
+except:
+    print("any other error would actually be caught here")
+finally:
+    print("any code in a finally will always run, always")
 
+print("You can also raise your own errors. In other words, code exceptions that might happen, so that if someone writes something the code isn't supposed to do, you will manually alert the user of such error")
+
+var1 = 10
+var2 = "HELLO WORLD"
+if isinstance(var2,str):
+    raise TypeError("var 2 nees to be an integer")
+
+
+try:
+    num = 5/0
+except:
+    print("An error occured")
+    raise #Even though the exception is caught here, using raise by itself will actually print out the exception message that was generated when the error was caught
