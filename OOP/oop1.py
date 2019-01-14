d86440ad2f872482689054be804db601d9c220eb
dasaed <carlos.dasaed@gmail.com>
2019-01-14 11:26:36 -0330

    adding more info

--- OOP/oop1.py
+++ OOP/oop1.py
@@ -1,8 +1,16 @@
 #Object Oriented Programming
-class Cat:
-	def __init__(self, color, legs)
+class Cat: #Class = object
+	def __init__(self, color, legs) # most important method of a class, always include it
 		self.color = color
 		self.legs = legs
+
 felix = Cat("ginger",4)
 rover = Cat("dog-colored",4)
 stumpy = Cat("brown",3)
+
+print("This is the color of felix")
+print(felix.color)
+print("# of legs for stumpy")
+print(stumpy.legs)
+
+
