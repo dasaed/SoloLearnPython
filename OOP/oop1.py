66b528ab98524bd227ef9731a42b5f57146ca5d7
dasaed <carlos.dasaed@gmail.com>
2019-01-14 11:14:41 -0330

    adding oop folder

---
+++ OOP/oop1.py
@@ -0,0 +1,8 @@
+#Object Oriented Programming
+class Cat:
+	def __init__(self, color, legs)
+		self.color = color
+		self.legs = legs
+felix = Cat("ginger",4)
+rover = Cat("dog-colored",4)
+stumpy = Cat("brown",3)
